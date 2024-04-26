a40a2a1c2c97cdf921487979c133af427eb46024
Century-ss <73206421+Century-ss@users.noreply.github.com>
2024-04-26 17:27:22 +0900

    👍 update to information message

--- src/extension.ts
+++ src/extension.ts
@@ -1,97 +1,137 @@
 import * as vscode from 'vscode'
 
+let isInProgress: boolean = false
+
 export function activate(context: vscode.ExtensionContext) {
-  let isInProgress: boolean = false
-  let isBreakTime: boolean = false
+  const startButtonText: string = 'Mob start'
+  let intervalTime: number
 
   const startButton = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, 0)
   startButton.command = 'mob-programming-timer.start'
-  startButton.text = 'モブプロを始める'
-  context.subscriptions.push(startButton)
+  startButton.text = startButtonText
   startButton.show()
 
   const stopButton = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, 0)
-  stopButton.command = 'mob-programming-timer.stop'
-  stopButton.text = 'モブプロを終了'
-  context.subscriptions.push(stopButton)
+  stopButton.command = 'mob-programming-timer.finish'
+  stopButton.text = 'Mob finish'
 
   const startDisposable = vscode.commands.registerCommand(
     'mob-programming-timer.start',
     async () => {
       isInProgress = true
 
-      const numberOfPeople = await vscode.window.showInputBox({
-        title: '何人でやりますか?(半角)',
+      const inputIntervalTime = await vscode.window.showInputBox({
+        title: 'How often do you change drivers in minutes?',
       })
-      const IntervalTime = await vscode.window.showInputBox({
-        title: '何分ごとに交代しますか(半角)?',
+
+      intervalTime = validateInputIntervalTime(inputIntervalTime)
+
+      startMobTimer({
+        intervalTime: intervalTime,
+        startButton: startButton,
+        stopButton: stopButton,
       })
-      // const breakTime = await vscode.window.showInputBox({
-      //   title: "1ごとに何分の休憩を入れますか?",
-      // });
-      if (IntervalTime === undefined || numberOfPeople === undefined) {
-        vscode.window.showInformationMessage('人数と時間を設定してください')
-        return
-      }
-
-      // let minute: number = parseInt(IntervalTime);
-      // let second: number = 0;
-      // TODO: 一旦、デバッグしやすいように秒数にしているが、後で分に変更する
-      let minute: number = 0
-      let second: number = parseInt(IntervalTime)
-
-      startButton.text = getDisplayTime(minute, second)
-      stopButton.show()
-
-      const intervalId = setInterval(() => {
-        if (!isInProgress) {
-          clearInterval(intervalId)
-          isBreakTime = false
-          return
-        }
-
-        if (second === 0) {
-          second = 60
-          minute = minute - 1
-        } else {
-          second = second - 1
-        }
-
-        if (minute === 0 && second === 0 && !isBreakTime) {
-          vscode.window.showInformationMessage('交代です 5秒後にタイマーを始めます')
-          isBreakTime = true
-          minute = 0
-          second = 5
-        }
-
-        if (minute === 0 && second === 0 && isBreakTime) {
-          // モブプロ再開
-          isBreakTime = false
-          // let minute: number = parseInt(IntervalTime);
-          // let second: number = 0;
-          // TODO: 一旦、デバッグしやすいように秒数にしているが、後で分に変更する
-          minute = 0
-          second = parseInt(IntervalTime)
-        }
-
-        startButton.text = getDisplayTime(minute, second)
-      }, 1 * 1000)
     }
   )
 
-  const stopDisposable = vscode.commands.registerCommand('mob-programming-timer.stop', () => {
-    isInProgress = false
-    console.log('stop')
+  const restartDisposable = vscode.commands.registerCommand('mob-programming-timer.restart', () => {
     stopButton.hide()
-    startButton.text = 'モブプロを始める'
+    vscode.window
+      .showInformationMessage("👨‍💻🔄👩‍💻 It's time to switch.", 'Start next mob', 'Finish mob')
+      .then((value) => {
+        if (value === 'Start next mob') {
+          startMobTimer({
+            intervalTime: intervalTime,
+            startButton: startButton,
+            stopButton: stopButton,
+          })
+        } else if (value === 'Finish mob') {
+          vscode.commands.executeCommand('mob-programming-timer.finish')
+        }
+      })
   })
 
+  const stopDisposable = vscode.commands.registerCommand('mob-programming-timer.finish', () => {
+    isInProgress = false
+    stopButton.hide()
+    startButton.text = startButtonText
+  })
+
+  context.subscriptions.push(startButton)
+  context.subscriptions.push(stopButton)
   context.subscriptions.push(startDisposable)
   context.subscriptions.push(stopDisposable)
+  context.subscriptions.push(restartDisposable)
 }
+
+export function deactivate() {}
 
 const getDisplayTime = (minute: number, second: number): string => {
   return String(minute) + ':' + String(second).padStart(2, '0')
 }
 
-export function deactivate() {}
+const zenkakuToHankaku = (word: string): string => {
+  return word.replace(/[Ａ-Ｚａ-ｚ０-９]/g, function (s: string) {
+    return String.fromCharCode(s.charCodeAt(0) - 0xfee0)
+  })
+}
+
+const getRemainingMinuteAndSecond = (intervalTime: number): [number, number] => {
+  // return [intervalTime, 0]
+  // TODO: 一旦、デバッグしやすいように秒数にしているが、後で分に変更する
+  return [0, intervalTime]
+}
+
+export const validateInputIntervalTime = (inputIntervalTime: string | undefined): number => {
+  if (inputIntervalTime === undefined) {
+    throw new Error('Please set the rotation time.')
+  }
+  if (isNaN(parseInt(zenkakuToHankaku(inputIntervalTime)))) {
+    throw new Error('Please enter an integer.')
+  }
+  if (parseInt(zenkakuToHankaku(inputIntervalTime)) === 0) {
+    throw new Error('Please enter an integer greater than zero.')
+  }
+  return parseInt(zenkakuToHankaku(inputIntervalTime))
+}
+
+type startMobTimerType = {
+  intervalTime: number
+  startButton: vscode.StatusBarItem
+  stopButton: vscode.StatusBarItem
+}
+
+export const startMobTimer = ({
+  intervalTime,
+  startButton,
+  stopButton,
+}: startMobTimerType): void => {
+  let minute: number
+  let second: number
+  ;[minute, second] = getRemainingMinuteAndSecond(intervalTime)
+
+  startButton.text = getDisplayTime(minute, second)
+  stopButton.show()
+
+  const intervalId = setInterval(() => {
+    if (!isInProgress) {
+      clearInterval(intervalId)
+      return
+    }
+
+    if (minute === 0 && second === 0) {
+      clearInterval(intervalId)
+      vscode.commands.executeCommand('mob-programming-timer.restart')
+      return
+    }
+
+    if (second === 0) {
+      second = 60
+      minute = minute - 1
+    } else {
+      second = second - 1
+    }
+
+    startButton.text = getDisplayTime(minute, second)
+  }, 1 * 1000)
+}
